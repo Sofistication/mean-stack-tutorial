198d7f10b4925958d42d074379576de2383a95fb
Sofistication <sordonez93@gmail.com>
2017-06-08 12:39:48 -0400

    add new post button
    - added function `$scope.addPost` to create generic post
    - used `ng-click` attribute to bind addPost to button

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ app.controller('MainCtrl', [
   '$scope',
   function($scope){
     $scope.posts = [
-      'post 1',
-      'post 2',
-      'post 3',
-      'post 4',
-      'post 5',
+      {title: 'post 1', upvotes: 5},
+      {title: 'post 2', upvotes: 2},
+      {title: 'post 3', upvotes: 13},
+      {title: 'post 4', upvotes: 4},
+      {title: 'post 5', upvotes: 7},
     ];
+    $scope.addPost = function () {
+      $scope.posts.push({title: 'A new post!', upvotes: 0});
+    };
   }
 ]);
